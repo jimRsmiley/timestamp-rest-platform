35990979c7f9949101a06f0c06f4fd34b4229d6c
Jim Smiley <skiver.kelvin@gmail.com>
2021-01-13 18:11:58 -0500

    adds route association to nat gw from app subnets

--- modules/network/route-tables.tf
+++ modules/network/route-tables.tf
@@ -13,3 +13,19 @@ resource "aws_route_table_association" "main" {
   subnet_id      = aws_subnet.public.*.id[count.index]
   route_table_id = aws_route_table.default.id
 }
+
+resource "aws_route_table" "private" {
+  count  = 2
+  vpc_id = aws_vpc.default.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.gw[count.index].id
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count = 2
+
+  subnet_id      = aws_subnet.app[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
