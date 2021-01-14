58bbe4a66900909c27908a9b738018c9fc248d6e
Jim Smiley <skiver.kelvin@gmail.com>
2021-01-14 12:21:39 -0500

    adds zipfile copy to init.sh script

--- init.sh
+++ init.sh
@@ -1,6 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
+SOURCE_ZIP_FILE=timestamp-app.zip
+SOURCE_S3_BUCKET=tf-codepipeline-source-timestamp-app
+
+mkdir -p tmp/
+
+# Provision the cloud infrastructure
 cd terraform
 
 terraform init
 terraform apply --auto-approve
+
+cd ..
+
+# Push the Node.js code up to the specified S3 bucket
+
+cd node-app
+zip -r ../tmp/${SOURCE_ZIP_FILE} *
+cd ..
+aws s3 cp tmp/${SOURCE_ZIP_FILE} s3://${SOURCE_S3_BUCKET}/${SOURCE_ZIP_FILE}
