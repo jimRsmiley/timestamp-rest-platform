d76f6548705e39206dd4855cf05ea695f5e7be18
Jim Smiley <skiver.kelvin@gmail.com>
2021-01-14 12:35:56 -0500

    init script runs the entire process

--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 SOURCE_ZIP_FILE=timestamp-app.zip
 SOURCE_S3_BUCKET=tf-codepipeline-source-timestamp-app
+CLUSTER_NAME=tf-cluster-timestamp-app-0
 
 mkdir -p tmp/
 
@@ -19,3 +22,9 @@ cd node-app
 zip -r ../tmp/${SOURCE_ZIP_FILE} *
 cd ..
 aws s3 cp tmp/${SOURCE_ZIP_FILE} s3://${SOURCE_S3_BUCKET}/${SOURCE_ZIP_FILE}
+
+# Deploy the K8S cluster
+CURRENT_REGION=`aws ec2 describe-availability-zones --output text --query 'AvailabilityZones[0].[RegionName]' | cat`
+aws eks --region ${CURRENT_REGION} update-kubeconfig --name ${CLUSTER_NAME}
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.43.0/deploy/static/provider/aws/deploy.yaml
+kubectl apply -f deployments
