d839c04ffa0ea9395ef32876243dfa3163b67876
Jim Smiley <skiver.kelvin@gmail.com>
2021-01-14 12:11:13 -0500

    allows setting a PORT env var in the node app

--- node-app/server.js
+++ node-app/server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 
 // Constants
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
 
 const STATIC_TEXT = process.env.APP_STATIC_TEXT || 'Automate all the things!';
