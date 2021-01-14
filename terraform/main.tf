f4c706264c56306482e6f05042f84a3d76a20896
Jim Smiley <skiver.kelvin@gmail.com>
2021-01-14 14:56:03 -0500

    refactors terraform provider file to be set up during github action

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,17 +11,6 @@ terraform {
   }
 }
 
-# 4893 is the IAM account; sandbox is 888458450351
-provider "aws" {
-  region = "us-east-1"
-
-  allowed_account_ids = ["888458450351"]
-
-  assume_role {
-    role_arn = "arn:aws:iam::888458450351:role/JsllcMainAccountAdministratorAccess"
-  }
-}
-
 module "network" {
   source       = "./modules/network"
   project_name = var.project_name
